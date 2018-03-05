fbff332d15e13e157f37b9947ce2b2628b2824d2
chrixz <elementrixz512@gmail.com>
2018-03-05 17:13:56 -0500

    Error en el consumo del servicio con Nodejs

--- src/components/places/PlaceHorizontal.js
+++ src/components/places/PlaceHorizontal.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Card from 'material-ui/Card';
+import {Card, CardHeader} from 'material-ui/Card';
 
 export default  class PlaceHorizontal extends React.Component{
   render(){
-    return(<h1 style={{'textAlign': 'left'}}>{this.props.place.title}</h1>);
+    return(
+      <Card style={{'marginTop':'1em', 'overflow': 'hidden'}}>
+         <div className="row">
+         <div className="PlaceH-avatar">
+          <img src={this.props.place.imageUrl } />
+         </div>
+         <div className="col-xs">
+          <CardHeader
+           title={this.props.place.title}
+           adress={this.props.place.address}
+          />
+         </div>
+         </div>
+      </Card>
+    );
   }
 }
--- src/requests/places.js
+++ src/requests/places.js
@@ -1,19 +1,23 @@
 export default {
   places: [
     {
-      imageUrl: '/images/place-3.jpg',
+      imageUrl: '/images/place-3.jpeg',
       title: 'Instalaciones',
-      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+
+      address: 'Av Principal #20'
     },
     {
       imageUrl: '/images/place-1.jpeg',
       title: 'Plantel Educativo',
-      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address: 'Av Principal #20'
     },
     {
       imageUrl: '/images/place-2.jpeg',
       title: 'Parque Recreativo',
-      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.'
+      description: 'Starbucks Corporation is an American coffee company and coffeehouse chain.',
+      address: 'Av Principal #20'
     }
   ]
 }
