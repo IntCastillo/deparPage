a8ec58418d9c2935bf3c58894e09c31aea66fb84
chrixz <elementrixz512@gmail.com>
2018-03-05 16:02:22 -0500

    Agregando [Rutas dinámicas]

--- src/Router.js
+++ src/Router.js
@@ -6,18 +6,33 @@ import {
 } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard'
 //import Signup from './pages/Signup'; Retirado Signup-page {Sólo Login.js}
 import App from './App';
 
+const  userSignedIn = false;
+
 export default class Router extends React.Component{
+   signedInRoutes(){
+     if(userSignedIn){
+       return(
+         <Route path="/new" render={()=><h1>Bienvenido</h1>} />
+       );
+     }
+   }
+    home(){
+       if(userSignedIn) return Dashboard;
+          return Home;
+       }
 
   render(){
     return(
     <ReactRouter>
        <App>
-         <Route exact path="/" component={Home}></Route>
+         <Route exact path="/" component={this.home()}></Route>
          <Route path="/login" component={Login}></Route>
          <Route path="/signup" component={Login}></Route>
+         {this.signedInRoutes()}
           </App>
       </ReactRouter>
       );
